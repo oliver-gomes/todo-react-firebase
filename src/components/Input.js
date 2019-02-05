29e5b4d03a80b992a03521f849f954ade5981a34
Oliver Gomes <ogomesphoto@gmail.com>
2019-02-05 13:28:34 -0500

    input update

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,12 +1,33 @@
 import React, { Component } from "react";
 import "./Input.css";
 
-export default function Input() {
-  return (
-    <input
-      type="text"
-      className="input"
-      placeholder="What needs to be done? "
-    />
-  );
+class Input extends Component {
+  state = {
+    newtodoContent: ""
+  };
+
+  handleUserInput = e => {
+    this.setState({
+      newtodoContent: e.target.value
+    });
+  };
+
+  writeTodo = () => {
+    this.setState({ newtodoContent: "" });
+  };
+  render() {
+    return (
+      <input
+        type="text"
+        className="input"
+        placeholder="What needs to be done? "
+        value={this.state.newtodoContent}
+        onChange={this.handleUserInput}
+        onClick={this.writeTodo}
+        addTodo={this.state.newtodoContent}
+      />
+    );
+  }
 }
+
+export default Input;
