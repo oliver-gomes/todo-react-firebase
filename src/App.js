9e2f385a1792af478d16ef0755e47245e3dfc846
Oliver Gomes <ogomesphoto@gmail.com>
2019-02-05 14:56:45 -0500

    function upgrade

--- src/App.js
+++ src/App.js
@@ -9,13 +9,14 @@ class App extends Component {
   state = {
     todos: [
       { id: 1, todoContent: "1st Todo" },
+      { id: 2, todoContent: "2nd Todo" },
       { id: 2, todoContent: "2nd Todo" }
     ]
   };
 
   addTodo = todo => {
     const previousTodo = this.state.todos;
-    previousTodo = push({ id: previousTodo.length + 1, todoContent: todo });
+    previousTodo.push({ id: previousTodo.length + 1, todoContent: todo });
     this.setState({
       todos: previousTodo
     });
@@ -29,12 +30,16 @@ class App extends Component {
         </Row>
 
         <Row>
-          <Input addTodo={this.addTodo} />
+          <Input addtodo={this.addTodo} />
         </Row>
 
         <Row>
           {this.state.todos.map(todo => (
-            <Todo key={todo.id} todoContent={todo.todoContent} />
+            <Todo
+              key={todo.id}
+              todoId={todo.id}
+              todoContent={todo.todoContent}
+            />
           ))}
         </Row>
       </Container>
